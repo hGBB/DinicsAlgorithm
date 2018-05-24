61a40e1a600517fdf13c70e24298a31fbafea326
orax <binder34@gw.uni-passau.de>
2018-05-24 22:29:25 +0200

    set structure packages

---
+++ Dinic/src/user_interaction/Shell.java
@@ -0,0 +1,21 @@
+package user_interaction;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public final class Shell {
+    public Shell() {
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader stdin = new BufferedReader(new InputStreamReader(System.in));
+
+    }
+
+
+
+
+
+
+}
