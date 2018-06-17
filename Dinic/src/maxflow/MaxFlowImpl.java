cd2e229e7db61767fdfd959d54dd86d255a09c54
orax <binder34@gw.uni-passau.de>
2018-06-17 15:15:56 +0200

    start on maxflow

--- Dinic/src/maxflow/MaxFlowImpl.java
+++ Dinic/src/maxflow/MaxFlowImpl.java
@@ -4,13 +4,21 @@ package maxflow;
  * {@inheritDoc}
  */
 public class MaxFlowImpl implements MaxFlow {
+
+    public MaxFlowImpl() {
+
+    }
+
     /**
      * {@inheritDoc}
      */
     @Override
     public void computeMaxFlow(Net net) {
-
+        ResidualNet residualNet = net.createResidualNet();
+        boolean reachabel = residualNet.isSinkReachableFromSource();
+        NiveauGraph niveauGraph = net.createNiveauGraph(residualNet);
     }
+
 
     /**
      * {@inheritDoc}
